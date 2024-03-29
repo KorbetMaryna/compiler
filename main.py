14b7bba5749ed5ef55de1d04344e4448d58c6e45
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2024-03-29 14:00:02 -0400

    parser

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class LexicalError(Exception):
     pass
 
-class SyntaxError(Exception):
+class ParsingError(Exception):
     pass
 
 class TokenType:
@@ -68,7 +68,74 @@ class Lexer:
             raise LexicalError('Lexical analysis error')
 
         return Token(TokenType.EOF, None)
+    
+#!Визначимо базові класи для вузлів AST
+class AST:
+    pass
 
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+#!Pеалізуємо клас Parser , який створюватиме AST
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    #!Помилка синтаксичного аналізу
+    def error(self):
+        raise ParsingError('Parse error')
+    
+    #!Порівнюємо поточний токен з очікуваним токеном і, якщо вони збігаються, поглинаємо його й переходимо до наступного токена.
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    #!Парсер для 'term' правил граматики. У нашому випадку - це цілі числа.
+    def term(self):
+        token = self.current_token
+        self.eat(TokenType.INTEGER)
+        return Num(token)
+
+    #!Парсер для арифметичних виразів.
+    def expr(self):
+        node = self.term()
+        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+            token = self.current_token
+            if token.type == TokenType.PLUS:
+                self.eat(TokenType.PLUS)
+            elif token.type == TokenType.MINUS:
+                self.eat(TokenType.MINUS)
+
+            node = BinOp(left=node, op=token, right=self.term())
+        return node
+
+#!Додамо метод print_ast для візуалізації структури AST
+def print_ast(node, level=0):
+    indent = '  ' * level
+    if isinstance(node, Num):
+        print(f"{indent}Num({node.value})")
+    elif isinstance(node, BinOp):
+        print(f"{indent}BinOp:")
+        print(f"{indent}  left: ")
+        print_ast(node.left, level + 2)
+        print(f"{indent}  op: {node.op.type}")
+        print(f"{indent}  right: ")
+        print_ast(node.right, level + 2)
+    else:
+        print(f"{indent}Unknown node type: {type(node)}")
+
+#!Модифікуємо функцію main для запуску нашого коду та використаємо функцію print_ast для побудови AST.
 def main():
     while True:
         try:
@@ -77,13 +144,11 @@ def main():
                 print("Exit the program.")
                 break
             lexer = Lexer(text)
-            token = lexer.get_next_token()
-            while token.type != TokenType.EOF:
-                print(token)
-                token = lexer.get_next_token()
+            parser = Parser(lexer)
+            tree = parser.expr()
+            print_ast(tree)
         except Exception as e:
             print(e)
 
 if __name__ == '__main__':
     main()
-
